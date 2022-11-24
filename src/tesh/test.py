8412342038278e3e0d0cd018d0a288f225f7af4b
Domen Kožar <domen@dev.si>
2022-11-24 14:08:51 +0000

    verbose: a bit better formatting

--- src/tesh/test.py
+++ src/tesh/test.py
@@ -22,7 +22,9 @@ def test(filename: str, session: ShellSession, verbose: bool) -> None:
             shell.expect(r"\$ ")
         for index, block in enumerate(session.blocks):
             if verbose:
-                print("      ", block)
+                print()
+                print("      Command: ", block.command)
+                print("      Output: ", block.output)
 
             expected_output = "\n".join(block.output)
 
