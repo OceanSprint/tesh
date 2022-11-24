1c7f55e8491aee1a9c07b6a20da949ba725ddb66
Domen Kožar <domen@dev.si>
2022-11-24 10:43:41 +0000

    Implement test

--- src/tesh/test.py
+++ src/tesh/test.py
@@ -1,20 +1,56 @@
-"""Run testable sessions line-by-line and assert the output."""
+import pexpect
+import os
+import sys
+import fnmatch
+from pathlib import Path
 
-# import pexpect
-# import os
-# from pathlib import Path
+def test(filename, session, verbose: bool):
+    with Path(filename).parent:
+        shell = pexpect.spawn('env -i PS1="$ " bash --norc --noprofile')
+        shell.expect("\$ ")
+        if session.setup:
+            shell.sendline(session.setup)
+            shell.expect("\$ ")
+        for i, block in enumerate(session.blocks):
+            if verbose:
+                print("      ", block)
 
-# def test(filename, session, verbose: bool):
-#     with Path(filename).parent:
-#         shell = pexpect.spawn(session.setup or 'sh')
-#         for block in session.blocks:
-#             if verbose:
-#                 print("      ", block)
-#             shell.sendline(block.command)
+            expected_output = "\n".join(block.output)
 
-#             if "DEBUG" in block.output:
-#                 shell.interact()
+            if "DEBUG" in expected_output:
+                print()
+                print("$ ", end="")
+                shell.send(block.command)
+                shell.interact()
 
-#             shell.expect("\n".join(block.output).replace("...", ".*"))
+            shell.sendline(block.command)
 
-#             # TODO: handle exit codes
+            shell.expect(block.command)
+            shell.expect("\$ ")
+
+
+            expected_match = expected_output.strip().replace("*", "[*]").replace("?", "[?]").replace("...", "*")
+            actual_output = shell.before.decode("utf-8").strip().replace('\r\n', '\n')
+
+
+            if not fnmatch.fnmatch(actual_output, expected_match):
+                print("❌ Failed")
+                print()
+                print("         Expected:")
+                print(expected_output)
+                print("         Got:")
+                print(actual_output)
+                sys.exit(1)
+
+            # handle exit codes
+            shell.sendline("echo $?")
+            shell.expect("echo [$][?]")
+            shell.expect("\$ ")
+            exitcode = int(shell.before.decode("utf-8").strip())
+            if session.exitcodes and exitcode != session.exitcodes[i]:
+                print("❌ Failed")
+                print()
+                print("         Expected exit code:", session.exitcodes[i])
+                print("         Got exit code:", exitcode)
+                sys.exit(1)
+
